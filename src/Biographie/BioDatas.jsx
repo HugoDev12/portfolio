945082c93be63a6252680797473e3b126dbb45d3
Occelli <occelli.hugo@hotmail.fr>
2021-11-28 23:37:33 +0100

    last changes on contents, dead line in 20mins

--- src/Biographie/BioDatas.jsx
+++ src/Biographie/BioDatas.jsx
@@ -5,10 +5,10 @@ function BioDatas(){
     const CONTENTS =
     [
         "Né sous le soleil des Bouches-du-Rhône, mon premier souvenir fût la pluie de la Normandie. Étant sous le signe astrologique du poisson, la ville d'Honfleur est mon bocal.",
-        "Dès l'enfance, je fus intéressé par la musique et les jeux vidéo. Pendant mon temps de loisir, je jonglais entre solfège et ordinateur. Cet attrait pour les jeux m'a amené vers le développement web.",
+        "Dès l'enfance, je fus intéressé par la musique et les jeux vidéo. Pendant mon temps de loisirs, je jonglais entre solfège et ordinateur. Cet attrait pour les jeux m'a amené vers le développement web.",
         "De nature forte et sensible, plus faiseur de projets que poète, j'ai tendance à être créatif et minutieux dans mon travail.",
         "Comprendre et créer une application ou un site web me provoque une certaine fierté et me pousse à apprendre davantage.",
-        "Amateur de pétanque et de natation, j'aime suivre l'actualité et les nouveautés technologiques."
+        "J'aime suivre l'actualité et les nouveautés technologiques."
     ]
 
     const LAYERCOLORS = 
